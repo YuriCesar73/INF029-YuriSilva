fca1c9516d6648c694c3d13172a76b0d4dddbc9a
Yuri Cesar <96021479+YuriCesar73@users.noreply.github.com>
2022-09-01 16:50:11 +0000

    .

--- .ccls-cache/@home@runner@INF029-YuriSilva/RevisitandoLógica@Decima.c
+++ .ccls-cache/@home@runner@INF029-YuriSilva/RevisitandoLógica@Decima.c
@@ -1,6 +1,6 @@
 # include <stdio.h>
 # include <string.h>
-# define tam 2
+# define tam 5
 /*Faça um programa que leia os dados de nome, idade, sexo e cpf de 5 pessoas. Ao final imprima tudo. 
 Utilize vetor e struct.*/
 
